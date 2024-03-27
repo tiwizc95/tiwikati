02c226d10a3ecaf141254ba4cf5184e27ef41ebf
tiwizc95 <86136212+tiwizc95@users.noreply.github.com>
2024-03-27 04:21:24 -0500

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -2,7 +2,7 @@ let Titulo = document.title;
 
 window.addEventListener('blur', () => {
     Titulo = document.title;
-    document.title = "Beia Dama";
+    document.title = "Mi Katita";
 })
 
 window.addEventListener('focus', () => {
